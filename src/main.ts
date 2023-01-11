ce108173d3b52be0dcf6d73a49d628e32b909e66
Nicholas Peters <95872411+Redicholas@users.noreply.github.com>
2023-01-11 17:18:53 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,50 @@
+import './style.css';
+
+const portfolioItemsDiv: HTMLDivElement | null = document.querySelector('#portfolioItems');
+
+const portfolioItems = [
+  {
+    title: 'Doer',
+    description: 'My todo list app. Made with Tailwind and TypeScript.',
+    imageUrl: '/img/Doer.png',
+    imgAlt: 'image of my todo list app',
+    link: 'https://redicholas.github.io/Doer/', 
+  },
+  {
+    title: 'KaffeHornet',
+    description: 'This is a group project for Medieinstitutet. <br> A semi-functional webshop.',
+    imageUrl: '/img/kaffeHornet.png',
+    imgAlt: 'image of my coffee corner website',
+    link: 'https://redicholas.github.io/KaffeHornet/',
+  },
+  {
+    title: 'To the Moon',
+    description: 'My first project for Medieinstitutet. HTML & CSS',
+    imageUrl: '/img/to-the-moon.png',
+    imgAlt: 'image of my space ventures website',
+    link: 'https://github.com/Redicholas/space-ventures',
+  },
+];
+
+function showPortfolioItems() {
+  portfolioItems.forEach((portfolioItem) => {
+    const { title, description, imageUrl, imgAlt, link } = portfolioItem;
+    if (portfolioItemsDiv != null) { 
+      portfolioItemsDiv.innerHTML += `
+      <div class="rounded-xl mb-10 p-8 bg-gradient-to-t from-middleGreen to-darkGreen hover:bg-gradient-to-b text-sm">
+          <a
+            href="${link}"
+            loading="lazy"
+            title="${title}"
+            target="_blank"
+            class=""
+          >
+          <img src="${imageUrl}" alt="${imgAlt}" class="rounded-xl m-auto mb-8 w-full">
+        </a>
+        <p class="text-center">${description}</p>
+      </div>
+      `;
+    }});
+}
+
+showPortfolioItems();
