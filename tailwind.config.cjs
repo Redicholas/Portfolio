5c50c1506830706205161e8028e98d8aaae683ec
Nicholas Peters <peters.nicholas@gmail.com>
2023-01-12 13:34:37 +0100

    Replace CV PDF with Modal

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      openSans: ['Comfortaa', 'sans-serif'],
-      josefin: ['Red Hat Text', 'serif'],
+      comfortaa: ["Comfortaa", "sans-serif"],
+      redHat: ["Red Hat Text", "serif"],
     },
     extend: {},
     colors: {
-      fire: '#f27405',
-      darkFire: '#731702',
-      white: '#fff',
-      black: '#000',
-      darkGreen: '#014040',
-      middleGreen: '#02635E',
-      lightGreen: '#03A678',
-      transparentBlack: '#00000033',
+      fire: "#f27405",
+      darkFire: "#731702",
+      white: "#fff",
+      black: "#000",
+      darkGreen: "#014040",
+      middleGreen: "#02635E",
+      lightGreen: "#03A678",
+      transparentBlack: "#00000033",
     },
     plugins: [],
   },
