17bc3d56e535454473fcf647bebb624d5eaf9ee8
Nicholas Peters <peters.nicholas@gmail.com>
2023-02-08 18:44:40 +0100

    change color theme

--- src/main.ts
+++ src/main.ts
@@ -70,11 +70,11 @@ function showPortfolioItems() {
         >
         <div class="rounded-xl max-w-md mb-10 mx-auto p-4 border dark:border-transparentWhite
         border-transparentBlack -z-10 fade-in transition-all duration-300
-         hover:bg-transparentBlack dark:hover:bg-transparentWhite
+         hover:bg-transparentBlack dark:hover:bg-transparentWhite dark:bg-greyblack bg-yellowbeige
           ">
           <h3 class="text-center mb-2 text-2xl h-16">${title}</h3>
             <img src="${imageUrl}" alt="${imgAlt}" 
-            class="rounded-xl m-auto mb-4 w-full max-w-xs border border-transparentBlack relative -z-10"
+            class="rounded-xl m-auto mb-4 w-full max-w-xs border border-transparentBlack relative"
             height="100" width="100">
             <p class="text-center text-sm">${description}</p>
         </div>
