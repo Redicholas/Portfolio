c0a7bbb9d103a9b5b0f4642d605a5f93e9853692
Nicholas Peters <peters.nicholas@gmail.com>
2023-02-12 13:19:36 +0100

    changed portfolio item links

--- src/main.ts
+++ src/main.ts
@@ -13,29 +13,25 @@ const portfolioItems = [
     description: "HTML | Sass | TypeScript | Vite",
     imageUrl: "/img/pizzaleria.png",
     imgAlt: "image of our pizza website",
-    link: "https://github.com/Medieinstitutet/fed22d-grafiska-verktyg-korsbarstomaterna",
+    repoLink: "https://github.com/Medieinstitutet/fed22d-grafiska-verktyg-korsbarstomaterna",
+    liveLink: "https://medieinstitutet.github.io/fed22d-grafiska-verktyg-korsbarstomaterna/",
   },
   {
     title: "Doer",
     description: "HTML | Tailwind CSS | TypeScript | Vite",
     imageUrl: "/img/Doer.png",
     imgAlt: "image of my todo list app",
-    link: "https://redicholas.github.io/Doer/",
+    repoLink: "https://github.com/Redicholas/Doer",
+    liveLink: "https://redicholas.github.io/Doer/"
   },
   {
     title: "KaffeHörnet",
     description: "HTML | Sass | JavaScript | Vite",
     imageUrl: "./img/kaffeHornet.png",
     imgAlt: "image of our coffee corner website",
-    link: "https://redicholas.github.io/KaffeHornet/",
+    repoLink: "https://github.com/Redicholas/KaffeHornet",
+    liveLink: "https://redicholas.github.io/KaffeHornet/",
   },
-  // {
-  //   title: 'To the Moon',
-  //   description: 'HTML | CSS',
-  //   imageUrl: '../img/to-the-moon.png',
-  //   imgAlt: 'image of my space ventures website',
-  //   link: 'https://github.com/Redicholas/space-ventures',
-  // },
 ];
 
 function toggleDarkLight() {
@@ -58,27 +54,23 @@ function closeCvModal() {
 
 function showPortfolioItems() {
   portfolioItems.forEach((portfolioItem) => {
-    const { title, description, imageUrl, imgAlt, link } = portfolioItem;
+    const { description, imageUrl, imgAlt, repoLink, liveLink } = portfolioItem;
     if (portfolioItemsDiv != null) {
       portfolioItemsDiv.innerHTML += `
-      <a
-        href="${link}"
-        loading="lazy"
-        title="${title}"
-        target="_blank"
-        class=""
-        >
-        <div class="rounded-xl max-w-md mx-auto p-4 border dark:border-transparentWhite
-        border-transparentBlack -z-10 fade-in transition-all duration-300
-         hover:bg-transparentBlack dark:hover:bg-transparentWhite dark:bg-greyblack bg-transparentWhite
-          ">
-          <h3 class="text-center mb-2 text-2xl h-16">${title}</h3>
-            <img src="${imageUrl}" alt="${imgAlt}" 
-            class="rounded-xl m-auto mb-4 w-full max-w-xs border border-transparentBlack relative"
-            height="100" width="100">
-            <p class="text-center text-sm">${description}</p>
-        </div>
-      </a>
+      <div class="rounded-xl max-w-md mx-auto p-4 border dark:border-transparentWhite
+      border-transparentBlack fade-in transition-all duration-300
+        hover:bg-transparentBlack dark:hover:bg-transparentWhite dark:bg-greyblack bg-transparentWhite
+        ">
+        
+          <img src="${imageUrl}" alt="${imgAlt}" 
+          class="rounded-xl m-auto mb-4 w-full max-w-xs border border-transparentBlack relative"
+          height="100" width="100">
+          <p class="text-center text-sm">${description}</p>
+          <div class="flex justify-around mt-4 relative">
+            <a href="${repoLink}" target="_blank" title="Link to the GitHub Repository" class="border dark:border-transparentWhite border-transparentBlack  px-6 py-1 rounded hover:bg-yellow hover:text-greyblack transition-all"/>Repo</a>
+            <a href="${liveLink}" target="_blank" title="Link to the live site"  class="border dark:border-transparentWhite border-transparentBlack  px-6 py-1 rounded hover:bg-yellow hover:text-greyblack transition-all"/>Live</a>
+          </div>
+      </div>
       `;
     }
   });
